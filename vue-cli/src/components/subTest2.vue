f3a31d2ecc62af328e2667095dae1a5d8dd77e96
csriioo <1344246051@qq.com>
2018-02-02 23:43:23 +0800

    vue-cli

---
+++ vue-cli/src/components/subTest2.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>我是subTset2</h1>
+        id:{{$route.params.id}}
+        name:{{$route.params.name}}
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
