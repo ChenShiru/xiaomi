4ff86f0f20e20c1179719b67046dcb8125481644
csriioo <1344246051@qq.com>
2018-04-13 01:05:43 +0800

    html5复习

--- 复习/requireJs/index.js
+++ 复习/requireJs/index.js
@@ -2,13 +2,19 @@
  * Created by csriioo on 2018/4/5.
  */
 /*
-require(['add'],function(add){
-        console.log(add(5,6));
-});*/
+ require(['add'],function(add){
+ console.log(add(5,6));
+ });*/
 require(['sortArray'],function(sortArray){
         var arr =[1,8,5,44,3,6,10];
         var obj ={
                 name:'zs'
-        }
+        };
         console.log(sortArray(obj));
 });
+
+/*
+* AMD:依赖前置
+* CMD：依赖就近
+*
+* */
