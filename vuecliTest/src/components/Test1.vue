6e75f882a024db5981a517695071c114ce9d4f4e
csriioo <1344246051@qq.com>
2018-04-19 00:44:23 +0800

    vue

---
+++ vuecliTest/src/components/Test1.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="test1">
+            <h2>我是测试页面</h2>
+            <router-link to="/subTest/">subTest</router-link>
+            <!-- <router-link :to="{name:'subTest2',params:{id:1,name:'xx'}}">subTest2</router-link> -->
+            <router-link to="/subTest2/5/lisi">subTest2</router-link>
+            <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .test1{
+        width: 200px;
+        height: 200px;
+        margin: 0 auto;
+        background: #ff0000;
+    }
+</style>
